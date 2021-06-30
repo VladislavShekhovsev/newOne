4c8df4d54c6e692c34d7a3fa79918f7e7d29e8f4
Vladislav <vladislav13457@gmail.com>
2021-06-30 19:28:52 -0400

    created new project

---
+++ src/test/java/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+    //TODO create maven project
+    //Add hello class in test /java
+    //Add .gitignore
+    // new - file = .gitignore
+    /*
+    .idea
+    target
+    *.iml
+
+     */
+    // then enable version control and make commit
+}
