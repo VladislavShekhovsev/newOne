60c099083222d31d1721d0fd9ecdce03abac20b0
Vladislav <vladislav13457@gmail.com>
2021-06-30 21:15:48 -0400

    third commit on Us102

--- src/test/java/Us102.java
+++ src/test/java/Us102.java
@@ -2,6 +2,7 @@ public class Us102 {
     public static void main(String[] args) {
         System.out.println("first commmit");
         System.out.println("second commit");
+        System.out.println("third commit");
 
     }
 }
