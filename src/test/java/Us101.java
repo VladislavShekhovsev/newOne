b50a5990a7e511ca5ce765d98652f367b19b1355
Vladislav <vladislav13457@gmail.com>
2021-06-30 20:20:46 -0400

    second  commit

--- src/test/java/Us101.java
+++ src/test/java/Us101.java
@@ -1,5 +1,6 @@
 public class Us101 {
     public static void main(String[] args) {
         System.out.println("making commit and publishing from branch");
+        System.out.println("making another commit");
     }
 }
